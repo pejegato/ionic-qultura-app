730d0ee96855a8f024a65e61ad6f91766b0129b9
rodrigo <ramirezretamalrodrigo@gmail.com>
2018-06-24 23:42:15 -0400

    Formateo, nuevos loaders, carga de datos usuario, refactor de codigo

---
+++ src/providers/user/user.ts
@@ -0,0 +1,14 @@
+import {Injectable} from "@angular/core";
+
+/*
+ Generated class for the UserProvider provider.
+
+ See https://angular.io/guide/dependency-injection for more info on providers
+ and Angular DI.
+ */
+@Injectable()
+export class UserProvider {
+
+    public datosUsuario: any
+
+}
